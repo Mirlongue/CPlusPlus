f32234baf983df08c53ecaa342c8fe45333cdd9f
Xiaohui Dong <tianfuyuming@outlook.com>
2026-06-26 21:54:28 +0800

    26 0626 dump

--- trick/dump/main.cpp
+++ trick/dump/main.cpp
@@ -40,7 +40,16 @@ void dump_buff(char * p, int size, std::string name) {
         std::copy(p, p + size, std::ostream_iterator<char>{ofs});
     }
     ofs.close();
+}
 
+void read_buff(char * p, std::string name) {
+    std::string prefix = "D:/Reposity_github/CPlusPlus/trick/dump/build/";
+    std::string path = prefix + name + ".bin";
+    std::ifstream ifs(path, std::ios::binary);
+    if (ifs) {
+        std::copy(std::istreambuf_iterator<char>{ifs}, std::istreambuf_iterator<char>{}, p);
+        ifs.close();
+    }
 }
 
 int main() {
@@ -50,6 +59,22 @@ int main() {
 
     char st[11] = "0123456789";
     dump_buff(st, 10, "st");
+    for(int i = 0; i < 10; i++) {
+        std::cout << st[i];
+        st[i]='0';
+    }
+    std::cout << std::endl;
+
+    for(int i = 0; i < 10; i++) {
+        std::cout << st[i];
+    }
+    std::cout << std::endl;
+
+    read_buff(st,"st");
+    for(int i = 0; i < 10; i++) {
+        std::cout << st[i];
+    }
+    std::cout << std::endl;
 
     return 0;
 }
