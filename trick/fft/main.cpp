0d3738fd4be05e93a2467a3db16d5d9b4b5fc425
Xiaohui Dong <tianfuyuming@outlook.com>
2026-07-01 22:05:08 +0800

    26 0701

--- trick/fft/main.cpp
+++ trick/fft/main.cpp
@@ -13,17 +13,17 @@ bool pre_process(double * & reA, double * & inA,
     string st0, st1;
     cin >> st0 >> st1;
 
-    for(int i = 0; ; i++) if (st0[i] != '0') {
+    for (int i = 0; ; i++) if (st0[i] != '0') {
         st0 = st0.substr(i);
         break;
     }
     cout << "st0: " << st0 << endl;
 
-    for(int i = 0; ; i++) if (st1[i] != '0') {
+    for (int i = 0; ; i++) if (st1[i] != '0') {
         st1 = st1.substr(i);
         break;
     }
-    cout << "st1: " << st1 << endl;       
+    cout << "st1: " << st1 << endl;
 
     len = 1;
     int lenTmp;
@@ -38,42 +38,42 @@ bool pre_process(double * & reA, double * & inA,
     reB = new double[len]{};
     inB = new double[len]{};
 
-    for(int i = 0; i < len; i++) {
-        if(i < len0) reA[i] = (double)st0[len0 - i - 1] - '0';
-        if(i < len1) reB[i] = (double)st1[len1 - i - 1] - '0';
+    for (int i = 0; i < len; i++) {
+        if (i < len0) reA[i] = (double)st0[len0 - i - 1] - '0';
+        if (i < len1) reB[i] = (double)st1[len1 - i - 1] - '0';
         inA[i] = inB[i] = 0.0;
     }
 
-    for(int i = 0 ; i < len; i++) printf("%.1f ",reA[i]);
+    for (int i = 0 ; i < len; i++) printf("%.1f ",reA[i]);
     printf("\n");
-    for(int i = 0 ; i < len; i++) printf("%.1f ",reB[i]);
+    for (int i = 0 ; i < len; i++) printf("%.1f ",reB[i]);
 
     return true;
 }
 
 void fft(double * reX, double * inX, int n, int flag, double * reTmp, double * inTmp) {
-    if(n == 1) return;
+    if (n == 1) return;
 
     double reWm = cos(2 * pi / n), inWm = sin(2 * pi / n);
-    if(flag) inWm = -inWm;
+    if (flag) inWm = -inWm;
     double reW = 1.0, inW = 0.0;
 
     int k, u, i;
-    for(k = 1, u = 0; k < n; k += 2, u++) {
+    for (k = 1, u = 0; k < n; k += 2, u++) {
         reTmp[u] = reX[k];
         inTmp[u] = inX[k];
     }
-    for(k = 2; k < n; k += 2) {
+    for (k = 2; k < n; k += 2) {
         reX[k/2] = reX[k];
         inX[k/2] = inX[k];
     }
-    for(k = u, i = 0; k < n && i < u; k++, i++) {
+    for (k = u, i = 0; k < n && i < u; k++, i++) {
         reX[k] = reTmp[i];
         inX[k] = inTmp[i];
     }
     fft(reX, inX, n / 2, flag, reTmp, inTmp);
     fft(reX + n / 2, inX + n / 2, n / 2, flag, reTmp, inTmp);
-    for(k = 0; k < n / 2; k++) {
+    for (k = 0; k < n / 2; k++) {
         int tag = k + n / 2;
         double reT = reW * reX[tag] - inW * inX[tag];
         double inT = reW * inX[tag] + inW * reX[tag];
@@ -111,7 +111,7 @@ void run_fft(double * reX, double * inX, int n, int flag) {
 
 int rev(int x, int ser) {
     int ans = 0;
-    for(int i = 0; i < ser; i++) {
+    for (int i = 0; i < ser; i++) {
         ans <<= 1;
         ans |= x & 1;
         x >>= 1;
@@ -121,20 +121,20 @@ int rev(int x, int ser) {
 
 void fft_(double * reA, double * inA, int n, int flag) {
     double lgn = log((double)n) / log((double)2);
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         int j = rev(i, lgn);
         if (j > i) {
             swap(reA[i], reA[j]);
             swap(inA[i], inA[j]);
         }
     }
-    for(int s = 1; s <= lgn; s++) {
+    for (int s = 1; s <= lgn; s++) {
         int m = 1 << s;
         double reWm = cos(2 * pi / m), inWm = sin(2 * pi / m);
         if (flag) inWm = -inWm;
-        for(int k = 0; k < n; k += m) {
+        for (int k = 0; k < n; k += m) {
             double reW = 1.0, inW = 0.0;
-            for(int j = 0; j < m / 2; j++) {
+            for (int j = 0; j < m / 2; j++) {
                 int tag = k + j + m / 2;
                 double reT = reW * reA[tag] - inW * inA[tag];
                 double inT = reW * inA[tag] + inW * reA[tag];
@@ -172,7 +172,7 @@ int main() {
         fft_(reA, inA, len, 0);
         fft_(reB, inB, len, 0);
 
-        for(int i = 0; i < len; i++) {
+        for (int i = 0; i < len; i++) {
             double reC = reA[i] * reB[i] - inA[i] * inB[i];
             double inC = reA[i] * inB[i] + inA[i] * reB[i];
             reA[i] = reC;
@@ -181,16 +181,16 @@ int main() {
         // run_fft(reA, inA, len, 1);
         fft_(reA, inA, len, 1);
 
-        for(int i = 0; i < len; i++) ans[i] = (int)(reA[i] + 0.5);
-        for(int i = 0; i < len; i++) {
+        for (int i = 0; i < len; i++) ans[i] = (int)(reA[i] + 0.5);
+        for (int i = 0; i < len; i++) {
             ans[i + 1] += ans[i] / 10;
             ans[i] %= 10;
         }
         int tag = len-1;
-        while(ans[tag] == 0 && tag > 0) tag--;
+        while (ans[tag] == 0 && tag > 0) tag--;
 
         printf("\n");
-        for(int i = tag; i >= 0; i--)
+        for (int i = tag; i >= 0; i--)
             printf("%d", ans[i]);
         printf("\n");
 
