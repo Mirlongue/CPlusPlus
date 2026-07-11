98c60fb62081ef7a367b3bd40b0c2ec54bd0cb8c
Xiaohui Dong <tianfuyuming@outlook.com>
2026-07-12 02:18:25 +0800

    stub 26 07 12

--- trick/stub/main.cpp
+++ trick/stub/main.cpp
@@ -15,33 +15,42 @@ public:
     int getX() {
         return x_;
     }
+    friend int A::* fgetX(A) {
+        return &A::x_;
+    }
 };
 
-// template <typename PtrType, PtrType PtrValue, typename TagType>
-// struct private_access {
-//     friend PtrType get(TagType) { return PtrValue; }
-// };
 
-// struct PrivateAccessTag__COUNTER__ {};
-// template struct private_access<decltype(&A::x_), &A::x_,
-//         PrivateAccessTag__COUNTER__>;
-
-// int A::* get(PrivateAccessTag__COUNTER__);
 
 ACCESS_PRIVATE_FIELD(A, int, x_);
+// namespace {
+//     struct PrivateAccessTag6 {};
+//     template struct private_access<decltype(&A::x_), &A::x_, PrivateAccessTag6>;
+//     using Alias_PrivateAccessTag6 = int;
+//     using PtrType_PrivateAccessTag6 = Alias_PrivateAccessTag6 A::*;
+//     PtrType_PrivateAccessTag6 get(PrivateAccessTag6);
+// }
+// namespace {
+//     int & Ax_(A &&t) { return t.*get(PrivateAccessTag6{}); }
+//     int & Ax_(A &t) { return t.*get(PrivateAccessTag6{}); }
+//     using XPrivateAccessTag6 = int;
+//     using YPrivateAccessTag6 = const XPrivateAccessTag6;
+//     YPrivateAccessTag6 & Ax_(const A &t) { return t.*get(PrivateAccessTag6{}); }
+// }
 
 int main() {
 
     A a;
-    a.setX(4);
-    cout << a.getX() << endl;
 
-    // int & A_x = a.*get(PrivateAccessTag__COUNTER__{});
+    a.setX(0);
+    cout << a.getX() << endl;
 
     int & A_x = Ax_(a);
-
-    A_x = 5;
+    A_x = 1;
     cout << a.getX() << endl;
+
+    A_x = 2;
+    cout << a.*fgetX(a) << endl;
 
     return 0;
 }
